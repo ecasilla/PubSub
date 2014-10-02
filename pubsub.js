0a35bb56ce3398efef3cda0b20fdb03b5d20c02a
Ernie Casilla <ecasilla@icloud.com>
2014-10-02 13:17:44 -0300

    added documentation

--- pubsub.js
+++ pubsub.js
@@ -1,5 +1,19 @@
-PubSub = {handlers:{}}
+/**
+ ** A module that helps with basic events
+ ** @{@link https://github.com/ecasilla/PubSub}
+ ** @author Ernie Casilla
+ ** @license MIT
+ ** @module PubSub
+ **/
 
+
+PubSub = {handlers:{}}
+/**
+ ** generate a handler for any named event
+ ** @param {string} the name of event 
+ ** @param {function} a callback handler for that event
+ ** @return { object } return the new PubSub Object
+ **/
 PubSub.on = function(eventType, handler) { 
   if (!(eventType in this.handlers)) {
     this.handlers[eventType] = [];
@@ -8,6 +22,11 @@ PubSub.on = function(eventType, handler) {
   return this; 
 }
 
+/**
+ ** Emit a named event
+ ** @param {string} the name of event 
+ ** @return { object } return the new PubSub Object
+ **/
 PubSub.emit = function(eventType) {
   var handlerArgs = Array.prototype.slice.call(arguments, 1); 
   for (var i = 0; i < this.handlers[eventType].length; i++) {
